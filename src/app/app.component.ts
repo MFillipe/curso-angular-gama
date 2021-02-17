3c65af7b8c964b57644de91c479a932948d45adc
Matheus Filipe <matheus_filipe00@outlook.com>
2021-02-17 19:58:52 -0300

    feat: adicionado life cycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
   title: string = 'Welcome';
 
   pudim: string = 'Ferrari';
 
-  constructor() {
-    setTimeout(() => {
-      return this.title = 'Welcome guys!! =)';
-    }, 5000);
-  }
+  showHeader: boolean = true;
+
+  constructor() {}
+  
+  ngOnInit() {}
 
   eventoRecebido($event) {
     console.log('AppComponent: EVENTO RECEBIDO!', $event);
